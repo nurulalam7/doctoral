27e5d7f1aea591f04ca99d9090ed1fa54c7ce9d7
nurulalam7 <nurulalam7540@gmail.com>
2022-03-13 17:16:31 +0600

    add

--- src/Pages/Header.js
+++ src/Pages/Header.js
@@ -4,11 +4,12 @@ import { useForm } from 'react-hook-form';
 import ima from '../Assets/ima1.jpg'
 
 const Header = () => {
- const{register,handlesubmit}=useForm();
+  const {register,handleSubmit}=useForm();
 
-  const onSubmit= data =>{
-    console.log("data")
-  };
+  const onSubmit=data =>{
+    console.log(data)
+  }
+
 
   
   
@@ -34,9 +35,9 @@ const Header = () => {
                   <h5 className='table-row'>Find your own desire nearby doctor which you need for the first aid and any serious desiase and you can find your best doctors </h5>
                 </div>
                 <div >
-                  <form onSubmit={handlesubmit(onSubmit)} className='flex grid grid-cols-6 my-20 gap-2 ' action="" >
-                    <input {...register("location")} className='col-span-3  rounded py-2 px-4' type="text" name='name'  placeholder='search by name' />
-                    <input className='col-span-2  rounded py-2 px-4' type="text" name='location' placeholder='search by location' />
+                  <form onSubmit={handleSubmit(onSubmit)} className='flex grid grid-cols-6 my-20 gap-2 ' action="" >
+                    <input {...register("name")}  className='col-span-3  rounded py-2 px-4' type="text" name='name'  placeholder='search by name' />
+                    <input {...register("location")} className='col-span-2  rounded py-2 px-4' type="text" name='location' placeholder='search by location' />
                     <button type='submit' className='bg-emerald-500 rounded-full'>search</button>
                   </form>
                  
